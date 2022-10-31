dc148630f3b5f32940e62ea6de1350bce8f9bf0d
malvarez88 <marianoalvarez66@gmail.com>
2022-10-31 20:51:03 -0300

    started

---
+++ components/Header.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <div>
+            <Image/>
+        </div>
+    </header>
+  )
+}
+
+export default Header
