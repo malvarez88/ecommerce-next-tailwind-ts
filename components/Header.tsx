247f0661c0b33e7429182db4864cf759d304790e
malvarez88 <marianoalvarez66@gmail.com>
2022-11-01 16:04:48 -0300

    fonts

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,20 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import {
+  SearchIcon,
+  ShoppingBagIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
 
 const Header = () => {
+  const session = false;
+
   return (
     <header className="sticky top-0 z-30 flex w-full items-center justify-between bg-[#E7ECEE] p-4">
       <div className="flex items-center justify-center md:w-1/5">
         <Link href="/">
-          <div className="relative h-10 w-5 cursor-pointer opacity-75 transition hover:opacity-100 ">
+          <div className="relative h-10 w-5 cursor-pointer opacity-75 transition hover:opacity-100">
             <Image
               src="https://rb.gy/vsvv2o"
               alt="header"
@@ -24,6 +31,33 @@ const Header = () => {
         <a className="headerLink">Explore</a>
         <a className="headerLink">Support</a>
         <a className="headerLink">Business</a>
+      </div>
+      <div className="flex items-center justify-center gap-x-4 md:w-1/5">
+        <SearchIcon className="headerIcon" />
+        <Link href="/checkout">
+          <div className="relative cursor-pointer ">
+            <span className="absolute -right-1 -top-1 z-50 flex h-4 w-4 items-center justify-center rounded-full bg-gradient-to-r from-pink-500 to-violet-500">
+              5
+            </span>
+            <ShoppingBagIcon className="headerIcon" />
+          </div>
+        </Link>
+
+        {session ? (
+          <Image
+            src={
+              // session.user?.image ||
+              "https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes-thumbnail.png"
+            }
+            alt=""
+            className="cursor-pointer rounded-full"
+            width={34}
+            height={34}
+            // onClick={}
+          />
+        ) : (
+          <UserIcon className="headerIcon" />
+        )}
       </div>
     </header>
   );
