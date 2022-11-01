c3d21e8e06990ae557236d836054a56078b68db6
malvarez88 <marianoalvarez66@gmail.com>
2022-11-01 17:05:28 -0300

    product section started

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import Header from '../components/Header'
-import Landing from '../components/Landing'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Header from "../components/Header";
+import Landing from "../components/Landing";
 
 const Home: NextPage = () => {
   return (
@@ -12,13 +12,20 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Header/>
+      <Header />
 
-      <main>
+      <main className="relative h-[200vh] bg-[#E7ECEE]">
         <Landing />
       </main>
+      <section className="relative z-40 -mt-[100vh] min-h-screen bg-[#1B1B1B]">
+        <div className="space-y-10 py-16">
+        <h1 className="text-4xl text-center font-medium tracking-wide text-white md:text-5xl">
+          New Promos
+        </h1>
+        </div>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
