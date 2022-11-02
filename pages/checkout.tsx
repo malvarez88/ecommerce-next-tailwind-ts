cf81bae105892989ad050ce81e435fb80f15c6d1
malvarez88 <marianoalvarez66@gmail.com>
2022-11-02 13:32:31 -0300

    checkout pay started

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -3,12 +3,15 @@ import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import Button from "../components/Button";
+import Currency from "react-currency-formatter";
 import CheckoutProduct from "../components/CheckoutProduct";
 import Header from "../components/Header";
-import { selectBasketItems } from "../state/basket";
+import { selectBasketItems, selectBasketTotal } from "../state/basket";
+import { ChevronDownIcon } from "@heroicons/react/outline";
 
 const Checkout = () => {
   const items = useSelector(selectBasketItems);
+  const basketTotal = useSelector(selectBasketTotal);
   const router = useRouter();
   const [itemsInBasket, setItemsInBasket] = useState(
     {} as { [key: string]: Product[] }
@@ -42,11 +45,61 @@ const Checkout = () => {
           )}
         </div>
         {items.length > 0 && (
-            <div className="mx-5 md:mx-8">
-                {Object.entries(itemsInBasket).map(([key, items]) => (
-                    <CheckoutProduct key={key} items={items} id={key} />
-                ))}
+          <div className="mx-5 md:mx-8">
+            {Object.entries(itemsInBasket).map(([key, items]) => (
+              <CheckoutProduct key={key} items={items} id={key} />
+            ))}
+
+            <div className="my-12 mt-6 ml-auto max-w-3xl">
+              <div className="divide-y divide-gray-300">
+                <div className="pb-4">
+                  <div className="flex justify-between">
+                    <p>Subtotal</p>
+                    <p>
+                      <Currency quantity={basketTotal} currency="USD" />
+                    </p>
+                  </div>
+                  <div className="flex justify-between">
+                    <p>Shipping</p>
+                    <p>FREE</p>
+                  </div>
+                  <div className="flex justify-between">
+                    <div className="flex flex-col gap-x-1 lg:flex-row">
+                      Estimated tax for:{" "}
+                      <p className="flex cursor-pointer items-end text-blue-500 hover:underline">
+                        Enter zip code
+                        <ChevronDownIcon className="h-6 w-6" />
+                      </p>
+                    </div>
+                    <p>$ -</p>
+                  </div>
+                </div>
+                <div className="flex justify-between pt-4 text-xl font-semibold">
+                  <h4>Total</h4>
+                  <h4>
+                    <Currency quantity={basketTotal} currency="USD" />
+                  </h4>
+                </div>
+              </div>
+              <div className="my-14 space-y-4">
+                <h4 className="text-xl font-semibold">
+                  How would you like to checkout
+                </h4>
+                <div className="flex flex-col gap-4 md:flex-row">
+                  <div className="order-2 flex flex-1 flex-col items-center rounded-xl bg-gray-200 p-8 py-12 text-center">
+                    <h4 className="mb-4 flex flex-col text-xl font-semibold">
+                      <span>Pay Monthly </span>
+                      <span>with Apple Card </span>
+                      <span>
+                        $ {Math.round(basketTotal / 36)}/mo. at 0% APR
+                        <sup className="-top-1"> ~</sup>
+                      </span>
+                    </h4>
+                  </div>
+                </div>
+              </div>
             </div>
+          </div>
         )}
       </main>
     </div>
